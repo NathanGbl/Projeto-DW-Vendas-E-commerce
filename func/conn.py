36d7fe3f2a9648e33302264b8b1100876153078f
Nathan Gabriel da Fonseca Leite <nathangabriel.f.leite@gmail.com>
2025-09-15 16:58:30 -0300

    Iniciando a conexão com o banco para poder fazer a inserção inicial de dados no banco OLTP.

--- func/conn.py
+++ func/conn.py
@@ -1,1 +1,13 @@
 from sqlalchemy import create_engine
+
+def nova_conexao(
+    user:str,
+    senha:str,
+    servidor:str,
+    porta:str,
+    driver:str,
+    nome_banco:str
+    ):
+    create_engine(
+        f"mssql+pyodbc://{user}:{senha}@{servidor}:{porta}/{nome_banco}?driver={driver}"
+    )
